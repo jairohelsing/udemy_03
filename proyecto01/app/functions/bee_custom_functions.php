37c0202af7c7703ddba6dfc70909adc91a49a811
JairoPasuy <jairohelsing@hotmail.com>
2020-09-03 21:09:31 -0500

    Primerizo

--- proyecto01/app/functions/bee_custom_functions.php
+++ proyecto01/app/functions/bee_custom_functions.php
@@ -2,9 +2,5 @@
 
 // Primera función de prueba custom
 function en_custom() {
-<<<<<<< HEAD
-  return 'ESTOY DENTRO DE CUSTOM_FUNCTION';
-=======
-  return 'ESTOY DENTRO DE CUSTOM_FUNCTION';
->>>>>>> 0fe1fce6318c2c06c2ca4e84227452f84ab09047
+  echo "hola";
 }
