f5e3013ecf2929857721c01b41e521be9bae5c05
JairoPasuy <jairohelsing@hotmail.com>
2020-09-03 21:08:42 -0500

    first commit

---
+++ proyecto01/app/functions/bee_custom_functions.php
@@ -0,0 +1,10 @@
+<?php 
+
+// Primera función de prueba custom
+function en_custom() {
+<<<<<<< HEAD
+  return 'ESTOY DENTRO DE CUSTOM_FUNCTION';
+=======
+  return 'ESTOY DENTRO DE CUSTOM_FUNCTION';
+>>>>>>> 0fe1fce6318c2c06c2ca4e84227452f84ab09047
+}
